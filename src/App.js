b27dcb9cb78901a8f0b2bffeef4bcf1d94755d8d
kaedaharakadzuha <amaliabest06@gmail.com>
2024-05-05 16:07:37 +0500

    немного поправила стили

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import './App.css';
 import FormPage from './pages/Form';
 import React from 'react';
 import myImage from './assets/image.png';
+import { ConfigProvider } from 'antd';
 
 function MyComponent(props, children) {
-  return <img height={'auto'} width={"100%"} style={{ overflow: "clip", position: "absolute", top: '0', right: '0' }} src={myImage} alt="My Image" />;
+  return <img height={'100%'} width={"100%"} style={{ overflow: "clip", position: "absolute", top: '0', right: '0' }} src={myImage} alt="My Image" />;
 }
 
 function App() {
   return (
-    <div className="App">
-      <MyComponent></MyComponent>
-      <section style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'absolute', width: '100vw', height: '100vh' }}>
-        <FormPage />
-      </section>
+    <ConfigProvider
+      theme={{
+        components: {
+          Input: {
+            colorPrimary: 'transparent',
+          }
+        },
+        token: {
+          // Seed Token
+          colorPrimary: 'transparent',
+          colorBgContainer: 'transparent',
+          colorBgMask: 'transparent',
+          colorPrimaryBorderHover: 'transparent',
+          colorBorder: 'transparent'
+        },
+      }}
+    >
+      <div className="App">
+        <MyComponent></MyComponent>
+        <section style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'absolute', width: '100vw', height: '100vh' }}>
+          <FormPage />
+        </section>
 
-    </div>
+      </div>
+    </ConfigProvider>
   );
 }
 
